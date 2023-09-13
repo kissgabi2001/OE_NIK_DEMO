c2e5488b06734e3c0c526fcd5e751d2310c8996e
kissgabi2001 <kissgabica2001@gmail.com>
2023-09-13 20:34:58 +0200

    first lesson tasks added

---
+++ SzTF1/01 - Gyakorlat/Gyakorlat_01/Gyakorlat_01/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakorlat_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1. FELADAT
+
+            //Írd ki a consolra, hogy Hello World
+
+            Console.WriteLine("1. FELADAT");
+
+
+            Console.WriteLine();
+
+            #endregion
+
+            #region 2. FELADAT
+
+            //Add be a programnak
+            //Üdvözöljön a consolon a program
+
+            Console.WriteLine("2. FELADAT");
+
+
+            Console.WriteLine();
+
+            #endregion
+
+            #region 3. FELADAT
+
+            //Adjunk meg egy tetszőleges számot, szorozzuk 10-zel
+            //Írjuk a ki az eredményt
+
+            Console.WriteLine("3. FELADAT");
+
+
+            Console.WriteLine();
+
+            #endregion
+
+            #region 4. FELADAT
+
+            //Változó átalakítás
+
+            Console.WriteLine("4. FELADAT");
+
+            //a, string
+
+            //b, parse
+
+            //c, kaszt - értékvesztéssel
+
+            Console.WriteLine();
+
+            #endregion
+
+            #region 5. FELADAT
+
+            // Add meg a téglalap a és b oldalát
+            // Számold ki a területét és a kerületét
+
+            Console.WriteLine("5. FELADAT");
+
+            Console.WriteLine();
+
+            #endregion
+
+            //--------------------------------------------------------------------------
+            // PLUSZ FELADATOK
+
+            #region 1. FELADAT -PLUSZ
+
+            // Téglalap terület és kerület számológép
+
+            Console.WriteLine("1. FELADAT");
+
+            Console.WriteLine();
+
+            #endregion
+
+            #region 2. FELADAT -PLUSZ
+
+            // Összeg- és szorzatszámológép
+
+            Console.WriteLine("2. FELADAT");
+
+            Console.WriteLine();
+
+            #endregion
+
+            #region 3. FELADAT -PLUSZ
+
+            //Készítsen alkalmazást, amely bekéri a felhasználó nevét és bruttó fizetését!
+            //Ezután írassuk ki:
+            //Kedves[XY] az ön számított nettó fizetése[ertek] Ft!
+            //Bruttó fizetés *0.66
+
+            Console.WriteLine("3. FELADAT");
+
+            Console.WriteLine();
+
+            #endregion
+        }
+    }
+}
