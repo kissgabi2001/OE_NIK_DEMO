e5c5b3f9157ccb998dd0adb4234321540bd29f79
kissgabi2001 <kissgabica2001@gmail.com>
2023-09-20 18:06:17 +0200

    Gyakorlat_02 tasks added

---
+++ SzTF1/02 - Gyakorlat/Gyakorlat_02/Gyakorlat_02/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakorlat_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1 feladat
+
+            //Kérj be a konzolról egy nevet és nézd meg, hogy a hallgató neve Béla-e
+
+
+            Console.WriteLine("\n");
+
+            #endregion
+
+            #region 2 feladat
+
+            //Vizsgáljuk meg, hogy a konzolról bekért szám pozitív, negatív, vagy nulla
+
+
+            Console.WriteLine("\n");
+
+            #endregion
+
+            #region 3 feladat
+
+            //Írj egy programot, amely segít egy személynek pénzügyi döntéseket hozni.
+            //Ha a bankszámla egyenlege nagyobb vagy egyenlő 100 000 egységgel, és a havi jövedelem legalább 5000 egység, akkor a program ajánlja a befektetés lehetőségét.
+            //Ha a bankszámla egyenlege kevesebb mint 100 000 egység, de a havi jövedelem legalább 3000 egység, akkor a program ajánlja a megtakarítási lehetőséget.
+            //Ha egyik feltétel sem teljesül, a program azt javasolja, hogy a felhasználó folytassa a jelenlegi pénzügyi helyzetének felülvizsgálatát és tervezését.
+
+            Console.WriteLine("\n");
+
+            #endregion
+
+            #region 4 feladat
+            //Elöltesztelő ciklus
+            //Készítsen programot, amely képernyőre írja az első 100 pozitív egész számot, de kihagyja közülük az öttel oszthatókat
+
+            Console.WriteLine("\n");
+
+            #endregion
+
+            #region 5 feladat
+
+            //Példa a hátultesztelő ciklusra
+            //Írj egy programot, amely lehetővé teszi számok megadását, de csak pozitív számokat fogad el
+
+            Console.WriteLine("\n");
+
+            #endregion
+
+            //---------------------------------
+            //          PLUSZ FELADATOK
+            //---------------------------------
+
+            #region 1 feladat
+
+            //Készíts egy egyszerű számológép programot. A programnak képesnek kell lennie az alábbi műveletek végrehajtására: összeadás, kivonás, szorzás és osztás.
+
+
+            #endregion
+
+        }
+    }
+}
