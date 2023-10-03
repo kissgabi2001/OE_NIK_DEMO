a50876f3f974a7cf4ee2c7e8d1142f8077960cca
kissgabi2001 <kissgabica2001@gmail.com>
2023-10-03 09:27:53 +0200

    Gyakorlat_04 tasks added

---
+++ SzTF1/04 - Gyakorlat/Gyakorlat_04/Gyakorlat_04/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakorlat_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region 1. feladat
+            //Egy két dimenziós tömbben tároljuk el  5 hét napjait az i.-ik sor j.-ik oszlopa jelentse az i. hét j. napján hány km-t futottunk,
+            //random számokkal töltsük fel a tömböt, majd határozzuk meg adot héten átlag hány km-t futottunk
+
+          
+            #endregion
+
+            #region 2. feladat
+            //Készíts programot, amely bemutatja a metódusok használatát!
+            //A programnak tartalmaznia kell két különböző metódust és egy Main metódust a futtatáshoz.
+            //Hozz létre egy Osszeadas nevű metódust, amely két egész számot kap bemenetként, összeadja őket, majd visszaadja az összeget.
+            //Hozz létre egy Koszontes nevű metódust, amely egy szöveget kap bemenetként(pl.egy nevet),
+            //és köszöntést jelenít meg a képernyőn, beleértve a megadott nevet.
+
+
+            #endregion
+
+            #region 3. feladat
+            //Valósítsa meg a következő metódusokat:
+            //1. int[] GenerateArray(int db) – Előállít egy db elemű tömböt, melynek elemeit feltölti 0 és 1000 közötti véletlen egész számokkal.
+            //2. string OutArray(int[] t) – A t tömb elemeit formázottan(egy sorba, vesszővel elválasztva) elhelyezi a kimeneti string-be(segédmetódus a tömb megjelenítéséhez).
+            //3. bool IsPrime(int num) – Megadja, hogy a bemeneti num változó prím-e.
+            //4. int NumOfPrimes(int[] t) – Meghatározza, hogy a t tömbben hány darab prímszám van.
+            //5. int GetMin(int[] t) – Meghatározza a t legkisebb elemének indexét.
+            //6. int NumOdds(int[] t) – Meghatározza, hogy a t tömbben hány darab páratlan szám van.
+
+           
+
+
+            #endregion
+
+            #region 4.feladat
+            //Adjunk meg két változót, majd cimzés szerinti értékadással cseréljük ki az eredetileg megadott értékeit
+
+            
+
+            // Címszerinti paraméterátadás a Swap metódussal
+
+
+            //FYI: Alapértelmezés szerint a C# érték szerinti paraméterátadást használ, ami azt jelenti,
+            //hogy egy metódus másolatot kap a bemeneti változóról, és az eredeti változót nem módosítja.
+            //A ref kulcsszó használatával azonban a metódus a változó memóriacímét kapja meg,
+            //így képes közvetlenül módosítani az eredeti változót.
+
+            #endregion
+
+        }
+
+        #region 2. feladat
+
+
+        #endregion
+
+        #region 3. feladat
+
+
+        #endregion
+
+        #region 4. feladat
+
+
+        #endregion
+
+
+    }
+}
